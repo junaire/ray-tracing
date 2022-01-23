a6c2437e52306d5bb5f8d339269600f4fd555cfe
Jun Zhang <jun@junz.org>
2022-01-23 19:55:17 +0800

    refactor: don't expose data members in Vector.
    Signed-off-by: Jun Zhang <jun@junz.org>

--- src/vec3.h
+++ src/vec3.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <cmath>
+
 #include "util.h"
 
 class Vec3 {
  public:
-  double e[3];
-
   constexpr Vec3() : e{0, 0, 0} {}
   constexpr Vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
@@ -56,25 +55,28 @@ class Vec3 {
   [[nodiscard]] constexpr double length() const {
     return std::sqrt(lengthSquared());
   }
+
+ private:
+  double e[3];
 };
 
 using Point3 = Vec3;
 using Color = Vec3;
 
 inline constexpr Vec3 operator+(const Vec3& u, const Vec3& v) {
-  return {u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]};
+  return {u.x() + v.x(), u.y() + v.y(), u.z() + v.z()};
 }
 
 inline constexpr Vec3 operator-(const Vec3& u, const Vec3& v) {
-  return {u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]};
+  return {u.x() - v.x(), u.y() - v.y(), u.z() - v.z()};
 }
 
 inline constexpr Vec3 operator*(const Vec3& u, const Vec3& v) {
-  return {u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]};
+  return {u.x() * v.x(), u.y() * v.y(), u.z() * v.z()};
 }
 
 inline constexpr Vec3 operator*(const Vec3& v, double t) {
-  return {t * v.e[0], t * v.e[1], t * v.e[2]};
+  return {t * v.x(), t * v.y(), t * v.z()};
 }
 
 inline constexpr Vec3 operator*(double t, const Vec3& v) { return v * t; }
@@ -82,14 +84,14 @@ inline constexpr Vec3 operator*(double t, const Vec3& v) { return v * t; }
 inline constexpr Vec3 operator/(const Vec3& v, double t) { return (1 / t) * v; }
 
 inline constexpr double dot(const Vec3& u, const Vec3& v) {
-  return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+  return u.x() * v.x() + u.y() * v.y() + u.z() * v.z();
 }
 
 inline constexpr Vec3 cross(const Vec3& u, const Vec3& v) {
   return {
-      u.e[1] * v.e[2] - u.e[2] * v.e[1],
-      u.e[2] * v.e[0] - u.e[0] * v.e[2],
-      u.e[0] * v.e[1] - u.e[1] * v.e[0],
+      u.y() * v.z() - u.z() * v.y(),
+      u.z() * v.x() - u.x() * v.z(),
+      u.x() * v.y() - u.y() * v.x(),
   };
 }
 
