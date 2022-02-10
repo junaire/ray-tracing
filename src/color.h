f15bb65e7f9565ec4624b7733cd725b487c9f4dc
Jun Zhang <jun@junz.org>
2022-02-10 19:29:19 +0800

    refactor: use std::clamp() and fmt::format
    Signed-off-by: Jun Zhang <jun@junz.org>

--- src/color.h
+++ src/color.h
@@ -1,20 +1,12 @@
 #pragma once
 
+#include <fmt/format.h>
+
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 
 #include "vec3.h"
-
-// make sure x is in the range of [min, max]
-inline double clamp(double x, double min, double max) {
-  if (x < min) {
-    return min;
-  }
-  if (x > max) {
-    return max;
-  }
-  return x;
-}
 
 void writeColor(std::ostream& os, const Color& pixel, int samplePerPixel) {
   auto r = pixel.x();
@@ -26,7 +18,8 @@ void writeColor(std::ostream& os, const Color& pixel, int samplePerPixel) {
   g *= std::sqrt(scale * g);
   b *= std::sqrt(scale * b);
 
-  os << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << " "
-     << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << " "
-     << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << "\n";
+  os << fmt::format("{} {} {}\n",
+                    static_cast<int>(256 * std::clamp(r, 0.0, 0.999)),
+                    static_cast<int>(256 * std::clamp(r, 0.0, 0.999)),
+                    static_cast<int>(256 * std::clamp(r, 0.0, 0.999)));
 }
